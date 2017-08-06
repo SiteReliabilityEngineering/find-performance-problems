32642c97c80b5753c8a374659299d94acd23f078
Koldo Oteo <koldo.oteo1@gmail.com>
2017-08-06 19:54:00 +0200

    Corrected the methods of the class SomePerf

--- koteo/someclasses.py
+++ koteo/someclasses.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #  Koldo Oteo Orellana (koldo.oteo@gmail.com)
-import os, time, glob
+import os, time, glob, re
+import psutil
 
 class SomePerf(object):
+
     proc_d = "/proc"
+
+    def __init__(self):
+        pass
+
+    # Find process in 'D' State
+    # Processes that are waiting for I/O are commonly in an "uninterruptible sleep" state or "D"
     def d_state_proc(self):
         try:
             for i in range(1, 9):
@@ -12,44 +20,27 @@ class SomePerf(object):
                 for stat in self.stat_f:
                     with open(stat) as f:
                         if 'disk sleep' in f.read():
-                            print ("pid: {} in D state".format(stat))
+                            find_p = re.search(r'\d+', stat)
+                            print ("The process with pid: {} in 'D' State. Try using iotop!".format(find_p.group(0)))
+                            print ("Trace the process, and see if it could be, a disk problem, cpu or network (nfs??)")
                 time.sleep(2)
         except FileNotFoundError as e:
             print ('{} ERROR!!!'.format(e))
             pass
 
-
-
-
-# Find process in 'D' State
-# Processes that are waiting for I/O are commonly in an "uninterruptible sleep" state or "D"
-def find_io():
-    loop_times = 1
-    try:
-        for i in range(1,6):
+    # How many open files you have in the system
+    # lsof -X -a  -d ^mem -d ^cwd -d ^rtd -d ^txt -d ^DEL |wc -l
+    # for p in /proc/[0-9]* ; do echo $(ls $p/fd | wc -l) $(cat $p/cmdline) ; done | sort -n  |awk '{ SUM += $1} END { print SUM }'
+    def files_open(self):
+        with open('/proc/sys/fs/file-max', 'r') as f:
+            self.fmax = f.read().strip()
+        self.files_lst = []
+        try:
             for proc in psutil.process_iter():
-                pinfo = proc.as_dict(attrs=['pid', 'name', 'status'])
-                if pinfo['status'] == 'disk-sleep':
-                    print ("The process: {}, with PID: {} is in 'D' State. Try using iotop!".format(pinfo['name'],
-                                                                                                    pinfo['pid']))
-                    print ("Look the process and see if it could be a disk problem, cpu or network (nfs??)")
-            time.sleep(2)
-    except psutil.NoSuchProcess:
-        pass
-
-# How many open files you have in the system
-# lsof -X -a  -d ^mem -d ^cwd -d ^rtd -d ^txt -d ^DEL |wc -l
-# for p in /proc/[0-9]* ; do echo $(ls $p/fd | wc -l) $(cat $p/cmdline) ; done | sort -n  |awk '{ SUM += $1} END { print SUM }'
-def files_open():
-    with open('/proc/sys/fs/file-max', 'r') as f:
-        fmax = f.read().strip()
-    files_lst = []
-    try:
-        for proc in psutil.process_iter():
-            files_lst.append(proc.num_fds())
-        print ("You have {} files open".format(sum(files_lst)))
-        if sum(files_lst) >= int(fmax):
-            print ("Take a look of your user's limit with ulimit -a, or maybe you need to increase your system "
-                   "limits(max open files). sysctl -w fs.file-max=NEW_VALUE")
-    except psutil.AccessDenied:
-print ("Login as root, or use sudo, to execute this Script!!!")
+                self.files_lst.append(proc.num_fds())
+            print ("You have {} files open".format(sum(self.files_lst)))
+            if sum(self.files_lst) >= int(self.fmax):
+                print ("Take a look of your user's limit with ulimit -a, or maybe you need to increase your system "
+                       "limits(max open files). sysctl -w fs.file-max=NEW_VALUE")
+        except psutil.AccessDenied:
+            print ("Login as root, or use sudo, to execute this Script!!!")
