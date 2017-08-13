e962e89012fc48728d73fdc018ab525bcde68f4e
SiteReliabilityEngineering <koldo.oteo1@gmail.com>
2017-08-13 13:47:07 +0200

    Modified __init__.py

--- koteo/__init__.py
+++ koteo/__init__.py
@@ -1,7 +1,6 @@
-from perf import Vmstat
-from perf import Iostat
-from someclasses import SomePerf
-from someclasses import Resources
-import somefunctions
-from someclasses import SomePerf
+from .perf import Vmstat
+from .perf import Iostat
+from .someclasses import SomePerf
+from .someclasses import Resources
+from .someclasses import SomePerf
 from .version import __version__
